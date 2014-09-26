af674afdc530c1bc5c96ff1eebcbf377ff4fd6a4
Falko Richter <falko@sensorberg.com>
2014-09-26 18:43:26 +0200

    new version of the SDK

--- 001_basic/src/main/java/com/sensorberg/sdk/demo/DemoActivity.java
+++ 001_basic/src/main/java/com/sensorberg/sdk/demo/DemoActivity.java
@@ -12,9 +12,7 @@ import com.sensorberg.sdk.scanner.Scanner;
 @SuppressWarnings("javadoc")
 public class DemoActivity extends Activity
 {
-	Scanner		scanner		= Scanner.getInstance();
-	Resolver	resolver	= Resolver.getInstance();
-    private TextView textView;
+	private TextView textView;
 
     @Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -31,17 +29,5 @@ public class DemoActivity extends Activity
 
 
         //You might want to inform the user, that btle is not supported on this device and all cool iBeacon features will not be accessible.
-		if(!scanner.isBluetoothSupported()) {
-            textView.setText("BTLE not supported");
-            Toast.makeText(this, "BTLE not supported.", Toast.LENGTH_LONG).show();
-		}
-        else if(!scanner.isBluetoothActive()){
-            textView.setText("bluetooth is not active on this device");
-            try {
-                scanner.requestBluetoothActivation(this);
-            } catch (SdkException e) {
-                //we ignore that btle might not be supported by this device
-            }
-        }
 	}
 }
--- 001_basic/src/main/java/com/sensorberg/sdk/demo/DemoApplication.java
+++ 001_basic/src/main/java/com/sensorberg/sdk/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.sensorberg.sdk.demo;
 import android.app.Application;
 import android.util.Log;
 
+import com.sensorberg.sdk.bootstrapper.BackgroundDetector;
+import com.sensorberg.sdk.bootstrapper.SensorbergApplicationBootstrapper;
 import com.sensorberg.sdk.presenter.PresenterConfiguration;
 import com.sensorberg.sdk.resolver.ResolverConfiguration;
 import com.sensorberg.sdk.scanner.ScannerConfiguration;
@@ -12,21 +14,19 @@ public class DemoApplication extends Application
 {
     private static final String TAG = "DemoApplication";
     private SensorbergApplicationBootstrapper boot;
+    private BackgroundDetector detector;
 
     @Override
 	public void onCreate() {
 		super.onCreate();
         Log.d(TAG, "onCreate application");
 
-
-		ScannerConfiguration scannerConfiguration = new ScannerConfiguration(this);
-        ResolverConfiguration resolverConfiguration = new ResolverConfiguration(this, "f257de3b91d141aa93b6a9b39c97b83df257de3b91d141aa93b6a9b39c97b83d");
-        PresenterConfiguration presenterConfiguration = new PresenterConfiguration(this);
-
-
+        PresenterConfiguration presenterConfiguration = new PresenterConfiguration(R.drawable.ic_launcher);
         //bootstrap the complete integration, keep a local reference to the bootstrapper
-        boot =  new SensorbergApplicationBootstrapper()
-                .bootstrapApplication(scannerConfiguration, resolverConfiguration, presenterConfiguration)
-                .bootstrapBackgroundScanning();
+        boot =  new SensorbergApplicationBootstrapper(this, presenterConfiguration, "f257de3b91d141aa93b6a9b39c97b83df257de3b91d141aa93b6a9b39c97b83d");
+
+        detector = new BackgroundDetector(boot);
+        registerActivityLifecycleCallbacks(detector);
+
 	}
 }
